2969f9f17ee62f9d9a3f66ff44a1379450740156
Mohamed Taha AMRIR <mta4917@gmail.com>
2022-01-18 03:03:06 +0100

    find long lat for plotting layer

---
+++ data_transforms/communs_long_lat.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("/home/lt/Cost_estimator/docs/communes-departement-region.csv")
+auction_data = pd.read_csv("/home/lt/Cost_estimator/project/filtered_data.csv")
+
+filter_on_region = df [df['nom_region']=='Île-de-France']
+
+#filter_on_region.to_csv("/home/lt/Cost_estimator/docs/communes-departement-iles-de-france.csv")
+
+
+auction_commun = auction_data['commune']
+
+def get_lat_long(x):
+    i=filter_on_region[filter_on_region["nom_commune_complet"]==x]
+    print("for ", x , "found " , i)
+
+auction_commun.apply(get_lat_long)
